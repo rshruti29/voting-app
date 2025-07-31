c5e28d4023117968b97f7f5cc292185b0bf0c49e
Shruti <shrutirathod2908@gmail.com>
2025-07-31 22:55:21 +0530

    debugging

--- server.js
+++ server.js
@@ -13,16 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(cors({
-  origin: "https://your-frontend.vercel.app",
-  credentials: true 
-}));
+
 
 const server = http.createServer(app);       
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5177', 'http://localhost:5176', 'http://localhost:5179'],
+    origin: 'https://voting-app-ten-sigma.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
@@ -40,8 +37,8 @@ io.on("connection", (socket) => {
 });
 
 app.use(cors({
-  origin: ['http://localhost:5177', 'http://localhost:5176', 'http://localhost:5179'],
-  credentials: true,
+  origin: "https://voting-app-ten-sigma.vercel.app/",
+  credentials: true ,
   methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
